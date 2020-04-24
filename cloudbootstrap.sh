48ce8b85671edaaa11952cf25f0d014565bae841
Justin Guese <guese.justin@gmail.com>
2020-04-24 14:28:26 +0200

    pip dependency added

--- cloudbootstrap.sh
+++ cloudbootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # aws ec2 deep learning ami
 git clone https://github.com/JustinGuese/tensortrade-experiments /home/ubuntu/tensortrade-experiments
-/home/ubuntu/anaconda3/envs/tensorflow2_p36/bin/pip install git+https://github.com/tensortrade-org/tensortrade stochastic ta
+/home/ubuntu/anaconda3/envs/tensorflow2_p36/bin/pip install git+https://github.com/tensortrade-org/tensortrade stochastic ta yfinance
